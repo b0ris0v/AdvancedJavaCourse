3367e2a1a56a0ddbf8d942e45dabdf26c37f460e
Borisov Vladislav <b0ris0v@yahoo.com>
2020-05-18 19:56:49 +0500

    Hibernate

--- src/main/java/web/dataSets/UserDataset.java
+++ src/main/java/web/dataSets/UserDataset.java
@@ -1,7 +1,18 @@
 package web.dataSets;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class UserDataset {
+    @Id
+    @Column(name="id")
     private long id;
+
+    @Column(name="name")
     private String name;
 
     public UserDataset(long id, String name) {
